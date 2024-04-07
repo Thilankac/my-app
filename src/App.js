9ff6f4a77ff9370359f98a31d50dc84115be40f5
Thilanka Chameera <Thilanka.Chameera@softlogiclife.lk>
2024-04-07 12:10:36 +0530

    project init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Add your h1 tag here with inline styling for purple color */}
+        <h1 style={{ color: 'purple' }}>Hello Team IT</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
